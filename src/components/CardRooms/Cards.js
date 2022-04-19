6fc8c6f7ddaacce88e3ec8fa113fa0e391ba7d36
Iyonissio <mbryoyo01@gmail.com>
2022-04-19 16:24:16 +0200

    updaete 19/2022

--- src/components/CardRooms/Cards.js
+++ src/components/CardRooms/Cards.js
@@ -3,9 +3,55 @@ import "./Cards.css";
 
 function Cards() {
   return (
-    <div className="topo-dia-mes">
-      <h1>Olaaa</h1>
-    </div>
+    <>
+      <div className="topo-dia-mes">
+        <div className="hoje">
+          <h1>Hoje: 100 000 MT</h1>
+        </div>
+        <div className="mes">
+          <h1>Mes: 1000 000 MT</h1>
+        </div>
+      </div>
+      {/* <hr className="dimensao" /> */}
+      <br />
+      <div class="container">
+        <ul>
+          <li>
+            <div class="bottom">Quarto 01</div>
+            <h2>01</h2>
+            <span>Ocupado</span>
+            <p>35 min</p>
+          </li>
+          <li></li>
+            <div class="bottom1">Quarto 02</div> 35 min{" "}
+          </li>
+          <li>
+            {" "}
+            <div class="bottom">Quarto 03</div> 35 min{" "}
+          </li>
+          <li>
+            {" "}
+            <div class="bottom">Quarto 04</div> 35 min{" "}
+          </li>
+          <li>
+            {" "}
+            <div class="bottom">Quarto 05</div> 35 min{" "}
+          </li>
+          <li>
+            {" "}
+            <div class="bottom">Quarto 06</div> 35 min{" "}
+          </li>
+          <li>
+            {" "}
+            <div class="bottom">Quarto 07</div> 35 min{" "}
+          </li>
+          <li>
+            {" "}
+            <div class="bottom">Quarto 08</div> 35 min{" "}
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
