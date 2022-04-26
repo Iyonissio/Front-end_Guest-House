644e1d45feb60047f14fc1d77b42ce2e376a893f
Iyonissio <mbryoyo01@gmail.com>
2022-04-26 10:47:44 +0200

    update

--- src/components/CardRooms/Cards.js
+++ src/components/CardRooms/Cards.js
@@ -1,292 +1,77 @@
 import * as React from "react";
 import "./Cards.css";
-import { Grid } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Imagem from "../../assets/room.jpeg";
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Grid from "@mui/material/Grid";
+//create our styles
+const classes = {
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 90,
+    backgroundColor: 'red',
+    padding: 20,
+    textAlign: "center",
+    color: "black",
+    fontWeight: 600,
+    fontFamily: "Roboto"
+  },
+  p: {
+    padding: 20,
+    color: "black",
+    fontWeight: 600,
+    fontFamily: "Roboto"
+  },
+};
 
 
 function Cards() {
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Quarto 01
-              </Typography>
-
-            </CardContent>
-            <CardActions>
-              <Button variant="contained">Contained</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Quarto 02
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Quarto 03
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={3} md={2.4} lg={2}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={Imagem}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
+      <div style={classes.root}>
+        <br />
+        <br />
+        <br />
+        <Paper elevation={2} >
+          <br />
+          <br />
+          <Container>
+            <Grid container spacing={3}>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4}>
+                  Quarto 1
+                  <br />
+                  <p style={classes.p}>35 min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4} >Quarto 2</Paper>
+              </Grid>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4} >Quarto 3</Paper>
+              </Grid>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4} >Quarto 4</Paper>
+              </Grid>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4} >Quarto 5</Paper>
+              </Grid>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4} >Quarto 6</Paper>
+              </Grid>
+              <Grid item xs={4} sm={2}>
+                <Paper style={classes.paper} elevation={4} >Quarto 7</Paper>
+              </Grid>
+            </Grid>
+          </Container>
+          <br />
+          <br />
+        </Paper>
 
 
-
-      </Grid>
-
-
-      {/* <div className="topo-dia-mes">
-        <div className="hoje">
-          <h1>Hoje: 100 000 MT</h1>
-        </div>
-        <div className="mes">
-          <h1>Mes: 1000 000 MT</h1>
-        </div>
       </div>
-      <hr className="dimensao" />
-      <br />
-      <div class="container">
-        <ul>
-          <li>
-            <div class="bottom">Quarto 01</div>
-            <h2>01</h2>
-            <span>Ocupado</span>
-            <p>35 min</p>
-          </li>
-          <li>
-            <div class="bottom1">Quarto 02</div> 35 min
-          </li>
-          <li>
-            <div class="bottom">Quarto 03</div> 35 min
-          </li>
-          <li>
-            <div class="bottom">Quarto 04</div> 35 min
-          </li>
-          <li>
-            <div class="bottom">Quarto 05</div> 35 min
-          </li>
-          <li>
-            <div class="bottom">Quarto 06</div> 35 min
-          </li>
-          <li>
-            <div class="bottom">Quarto 07</div> 35 min
-          </li>
-          <li>
-            <div class="bottom">Quarto 08</div> 35 min
-          </li>
-        </ul>
-      </div> */}
     </>
   );
 }
