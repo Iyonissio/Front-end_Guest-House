bef2f89ec98dc034c0935dea918f2c0f5d8af1fb
MalingasCUMBANE <malingascumbane1@gmail.com>
2022-06-17 13:51:26 +0200

    updating GUEST_HOUSE

--- src/components/CardRooms/Cards.js
+++ src/components/CardRooms/Cards.js
@@ -7,145 +7,240 @@ import Grid from "@mui/material/Grid";
 import Button from '@mui/material/Button';
 import QuartoFoto from './../../assets/room.jpeg';
 //create our styles
+
+// const classes = {
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     height: 100,
+//     backgroundColor: 'red',
+//     paddingBotton: 1,
+//     paddingTop: 20,
+//     textAlign: "center",
+//     color: "black",
+//     fontWeight: 600,
+//     fontFamily: "Roboto"
+//   },
+//   available: {
+//     height: 100,
+//     backgroundColor: 'white',
+//     paddingBotton: 1,
+//     paddingTop: 20,
+//     textAlign: "center",
+//     color: "black",
+//     fontWeight: 600,
+//     fontFamily: "Roboto"
+//   },
+//   p: {
+//     padding: 20,
+//     color: "black",
+//     fontWeight: 600,
+//     fontFamily: "Roboto"
+//   },
+//   pa: {
+//     color: "blue",
+//     fontWeight: 400,
+//     fontFamily: "Roboto"
+//   },
+// };
+
 const classes = {
   root: {
     flexGrow: 1,
   },
+
   paper: {
-    height: 100,
+    height: 80,
     backgroundColor: 'red',
-    paddingBotton: 1,
-    paddingTop: 20,
-    textAlign: "center",
+    textAlign: "left",
     color: "black",
-    fontWeight: 600,
-    fontFamily: "Roboto"
+    fontFamily: "Helvetica"
   },
+
   available: {
-    height: 100,
+    height: 80,
     backgroundColor: 'white',
-    paddingBotton: 1,
-    paddingTop: 20,
-    textAlign: "center",
+    textAlign: "left",
     color: "black",
-    fontWeight: 600,
+    fontWeight: 400,
     fontFamily: "Roboto"
   },
+
   p: {
-    padding: 20,
     color: "black",
-    fontWeight: 600,
-    fontFamily: "Roboto"
+    fontFamily: "Roboto",
+
   },
+
   pa: {
     color: "blue",
     fontWeight: 400,
     fontFamily: "Roboto"
   },
+
+  paragraph: {
+    fontSize: 25
+  }
 };
+
+
+function closeRoom() {
+  window.confirm("Deseja ocupar o quarto?")
+}
+
+function viewReports() {
+  alert("Ver relatórios.")
+}
+
 
 
 function Cards() {
   return (
     <>
       <div style={classes.root}>
-        <br />
-        <br />
-        <br />
-        <Paper elevation={2} >
+       
+        <Paper elevation={3} >
           <br />
-          <br />
+          
           <Container>
-            <Grid container spacing={3}>
-              <Grid item xs={4} sm={2}>
+          <div className="main-pricing">
+            <div className="today">Hoje: 17600</div>
+            <div className="month">Mês: 223400</div>
+          </div>
+          <br/>
+          <hr />
+          <br/>
+            <Grid container spacing={1}>
+              <Grid item xs={3} sm={1}>
                 <Paper style={classes.paper} elevation={4}>
-                  Quarto 1
+                  <p style={classes.paragraph}>&nbsp;01</p>
                   {/* <img src={QuartoFoto} /> */}
-                  <br />
-                  <p style={classes.p}>35 Min</p>
-                  <h1 style={classes.p}>jhk Min</h1>
+                  <p style={classes.p}>&nbsp;35min</p>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;03</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.available} elevation={4} >
-                  Quarto 2
-                  <br />
-                  <br />
-                  <Button size="small" style={classes.pa} variant="outlined">Disponivel</Button>
+                  <p style={classes.paragraph}>&nbsp;06</p>
+                  <Button size="small" style={classes.pa} onClick={closeRoom} fullWidth variant="text">Disponivel</Button>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.paper} elevation={4} >
-                  Quarto 3
-                  <br />
-                  <p style={classes.p}>35 Min</p>
+                  <p style={classes.paragraph}>&nbsp;04</p>
+                  <p style={classes.p}>&nbsp;35min</p>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
-                <Paper style={classes.paper} elevation={4} >
-                  Quarto 4
-                  <br />
-                  <p style={classes.p}>35 Min</p>
-                </Paper>
-              </Grid>
-              <Grid item xs={4} sm={2}>
-                <Paper style={classes.paper} elevation={4} >
-                  Quarto 5
-                  <br />
-                  <p style={classes.p}>35 Min</p>
-                </Paper>
-              </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.available} elevation={4} >
-                  Quarto 6
-                  <br />
-                  <br />
-                  <Button size="small" style={classes.pa} variant="outlined">Disponivel</Button>
+                  <p style={classes.paragraph}>&nbsp;06</p>
+                  <Button size="small" style={classes.pa} onClick={closeRoom} fullWidth variant="text">Disponivel</Button>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.paper} elevation={4} >
-                  Quarto 7
-                  <br />
-                  <p style={classes.p}>35 Min</p>
+                  <p style={classes.paragraph}>&nbsp;07</p>
+                  <p style={classes.p}>&nbsp;35min</p>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.paper} elevation={4} >
-                  Quarto 4
-                  <br />
-                  <p style={classes.p}>35 Min</p>
+                  <p style={classes.paragraph}>&nbsp;08</p>
+                  <p style={classes.p}>&nbsp;35min</p>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
-                <Paper style={classes.paper} elevation={4} >
-                  Quarto 5
-                  <br />
-                  <p style={classes.p}>35 Min</p>
-                </Paper>
-              </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.available} elevation={4} >
-                  Quarto 6
-                  <br />
-                  <br />
-                  <Button size="small" style={classes.pa} variant="outlined">Disponivel</Button>
+                  <p style={classes.paragraph}>&nbsp;02</p>
+                  <Button size="small" style={classes.pa} onClick={closeRoom} fullWidth variant="text">Disponivel</Button>
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={2}>
+              <Grid item xs={3} sm={2}>
                 <Paper style={classes.paper} elevation={4} >
-                  Quarto 7
-                  <br />
-                  <p style={classes.p}>35 Min</p>
+                  <p style={classes.paragraph}>&nbsp;09</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.available} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;10</p>
+                  <Button size="small" style={classes.pa} onClick={closeRoom} fullWidth variant="text">Disponivel</Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;11</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;12</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;13</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;14</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;15</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.available} elevation={4} >
+                  <p style={classes.paragraph}>16</p>
+                  <Button size="small" style={classes.pa} onClick={closeRoom} fullWidth variant="text">Disponivel</Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;17</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.available} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;18</p>
+                  <Button size="small" style={classes.pa} onClick={closeRoom} fullWidth variant="text">Disponivel</Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;19</p>
+                  <p style={classes.p}>&nbsp;35min</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Paper style={classes.paper} elevation={4} >
+                  <p style={classes.paragraph}>&nbsp;20</p>
+                  <p style={classes.p}>&nbsp;35min</p>
                 </Paper>
               </Grid>
             </Grid>
+            <br />
+            <br />
+            <Button size="small" style={classes.pa} onClick={viewReports} fullWidth variant="outlined">Relatórios</Button>
           </Container>
           <br />
           <br />
         </Paper>
-
-
       </div>
     </>
   );
